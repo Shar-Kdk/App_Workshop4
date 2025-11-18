b79d8be0db5d6c8e81d75bc05c87b32786b7af6f
Shar-Kdk <np05cp4a230249@iic.edu.np>
2025-11-18 09:27:31 +0545

    Added: Task-1

--- App_Workshop4/Program.cs
+++ App_Workshop4/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("======== Task 1: Student Class ========");
+            Student s1 = new Student { name = "Shar", age = 20, address = "Kathmandu" };
+            Student s2 = new Student { name = "Ram", age = 22, address = "Pokhara" };
+            s1.PrintDetails();
+            s2.PrintDetails();
+            Console.WriteLine("College: " + Student.college);
         }
     }
 }
---
+++ App_Workshop4/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Workshop4
+{
+    internal class Student
+    {
+        public string name;
+        public int age;
+        public string address;
+
+        public static string college = "IIC";
+
+        public void PrintDetails() => Console.WriteLine($"Name: {name}, Age: {age}, Address: {address}");
+    }
+}

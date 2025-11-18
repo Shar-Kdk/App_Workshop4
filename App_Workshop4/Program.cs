88eff7178c14ae6ba1a3d56972747ca95a840e75
Shar-Kdk <np05cp4a230249@iic.edu.np>
2025-11-18 09:02:47 +0545

    Add project files.

---
+++ App_Workshop4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace App_Workshop4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
